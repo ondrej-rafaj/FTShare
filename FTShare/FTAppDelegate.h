7b24e22f2b519b8400c26e3a47f09e651c079aff
Francesco Freezone <cescofry@gmail.com>
2011-10-19 10:41:31 +0100

    Initial Commit

---
+++ FTShare/FTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FTAppDelegate.h
+//  FTShare
+//
+//  Created by Francesco on 19/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FTShareTests/FTShareTests.h
@@ -0,0 +1,13 @@
+//
+//  FTShareTests.h
+//  FTShareTests
+//
+//  Created by Francesco on 19/10/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FTShareTests : SenTestCase
+
+@end
